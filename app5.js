e43bc7c0285e46923419eaad553cf9927bc4d993
ckdals82 <81973067+ckdals82@users.noreply.github.com>
2021-10-14 20:31:42 +0900

    삭제처리

--- app5.js
+++
@@ -1,6 +0,0 @@
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age)) {
-  //codition === true
-  console.log("숫자로 입력해줴요");
-}
--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,9 @@
-const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input");
+const toDoForm = document.getElementById('todo-form');
+const toDoInput = toDoForm.querySelector('input');
 // const toDoInput = document.querySelector("#todo-form input") 위랑 같은뜻
-const toDoList = document.getElementById("todo-list");
+const toDoList = document.getElementById('todo-list');
 
-const TODOS_KEY = "todos";
+const TODOS_KEY = 'todos';
 
 let toDos = [];
 
@@ -14,15 +14,18 @@ function saveToDos() {
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
-  const li = document.createElement("li");
-  const span = document.createElement("sapn");
-  span.innerText = newTodo;
-  const button = document.createElement("button");
-  button.innerText = "❌";
-  button.addEventListener("click", deleteToDo);
+  const li = document.createElement('li');
+  li.id = newTodo.id;
+  const span = document.createElement('sapn');
+  span.innerText = newTodo.text;
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li); //li를 ul 자식으로 만들어 출력시키기
@@ -31,13 +34,16 @@ function paintToDo(newTodo) {
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value; //들어오는 값을 new todo에 저장 아래에서 값을 비워도 상관없음.
-
-  toDoInput.value = ""; //값을 주기 엔터누르면 값 없어짐
-  toDos.push(newTodo); //toDosㄹㅏ는 array list에 들어오는값을 넣어줌
-  paintToDo(newTodo); //newTodo에 저장된 text값이 인자로  paintoDo에 넣어서 호출
+  toDoInput.value = ''; //값을 주기 엔터누르면 값 없어짐
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now()
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
-toDoForm.addEventListener("submit", handleToDoSubmit);
+toDoForm.addEventListener('submit', handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
